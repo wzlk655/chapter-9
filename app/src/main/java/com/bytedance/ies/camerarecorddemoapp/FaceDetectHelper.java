f9b7af9d30dbcf83cde2a68a1d35369e276c2b96
Steven <hepanlong@bytedance.com>
2019-01-25 22:41:40 +0800

    first commit

---
+++ app/src/main/java/com/bytedance/ies/camerarecorddemoapp/FaceDetectHelper.java
@@ -0,0 +1,88 @@
+package com.bytedance.ies.camerarecorddemoapp;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+public class FaceDetectHelper {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private static FaceDetectHelper mHelper = null;
+    private OnFaceDetectedCallback mFaceDetectedCallback = null;
+
+    private FaceDetectHelper() {
+        nativeInit();
+    }
+
+    public static FaceDetectHelper getHelper() {
+        synchronized (FaceDetectHelper.class) {
+            if (mHelper == null) {
+                synchronized (FaceDetectHelper.class) {
+                    if (mHelper == null) {
+                        mHelper = new FaceDetectHelper();
+                    }
+                }
+            }
+        }
+        return mHelper;
+    }
+
+    public void destroy() {
+        nativeUnInit();
+        mHelper = null;
+    }
+
+    public void setFaceDetectModelPath(String modelPath) {
+        if (!TextUtils.isEmpty(modelPath)) {
+            nativeSetFaceDetectModelPath(modelPath);
+        }
+    }
+
+    public void setLicense(String license) {
+        nativeSetLicense(license);
+    }
+
+    public void setFaceDetectedCallback(OnFaceDetectedCallback callback) {
+        mFaceDetectedCallback = callback;
+    }
+
+    public void writeBMP() {
+        nativeTestWriteBmp();
+    }
+
+    public interface OnFaceDetectedCallback {
+        void onFaceDetected(int ret);
+    }
+
+    public void detectFace(byte[] image, int pixelFormat, int width, int height, int stride) {
+        nativeDetectFace(image, pixelFormat, width, height, stride);
+    }
+
+    public void onFaceDetectedCallback(int ret) {
+        if (mFaceDetectedCallback != null) {
+            mFaceDetectedCallback.onFaceDetected(ret);
+        }
+    }
+
+    public static void nativeOnFaceDetectedCallback(int ret) {
+        Log.d("FaceDetectHelper", "JAVA detectFaceCallbackMethod ret : " + ret);
+        if (mHelper != null) {
+            mHelper.onFaceDetectedCallback(ret);
+        }
+    }
+
+    private native void nativeSetLicense(String license);
+
+    private native void nativeSetFaceDetectModelPath(String effectModePath);
+
+    private native void nativeInit();
+
+    private native void nativeUnInit();
+
+    private native void nativeDetectFace(byte[] image, int pixelFormat, int width, int height, int stride);
+
+    private native void nativeTestWriteBmp();
+}
